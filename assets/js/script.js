f3fc8b5b8a12460532c64ec1dbddada1e2c05373
datdiep754129 <dat.diep@indigames.net>
2024-04-04 17:06:21 +0700

    feat: disable inspect elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,3 +155,22 @@ for (let i = 0; i < navigationLinks.length; i++) {
     }
   });
 }
+
+//Disable inspect elements
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
